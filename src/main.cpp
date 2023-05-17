ae95e6c8dcfc057d22e7b5ca49055d1f3efb28a2
Joseph <eeroward@gmail.com>
2023-05-17 07:28:19 -0600

    Fully functional code point Added name_list.h into the cmake so that the code would finally run. At this point the code is fully function...finally.

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,6 @@
 #include "../include/Soldier.h"
 
 int main(){
-    // cout << "\n";
-    // string test = "Army Service Ribbon, Army Achievement Medal,";
-    // string spacer = "============================================\n";
-    // cout << "Test: " << test.size() << "\nSpacer: " << spacer.size() << "\n\n";
-    
     Soldier top = Soldier();
     top.srb();
-
-    // vector<string> test{"Foo", "Bar", "Malt", "Jule"};
-    // print_vect(test);
-    // std::cout << "\nSorting...\n";
-    // compare comp;
-    // std::sort(test.begin(), test.end());
-    // std::sort(test.begin(), test.end(), comp);
-    // print_vect(test);
-
-
-
 }
